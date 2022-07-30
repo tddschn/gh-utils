4141bff4e80f9832a76a0373cb6f103c1c5d4bb9
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-07-30 13:32:02 +0800

    Add windows support: looks for config dir under appdata

--- gh_utils/config.py
+++ gh_utils/config.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
-
+from . import NotFoundError
 from pathlib import Path
 
-DEFAULT_GH_CONFIG_YML_PATH = Path.home() / '.config/gh/config.yml'
-DEFAULT_GH_HOSTS_YML_PATH = Path.home() / '.config/gh/hosts.yml'
+DEFAULT_GH_CONFIG_DIR_UNIX = Path.home() / '.config/gh'
+DEFAULT_GH_CONFIG_DIR_WINDOWS = Path.home() / 'Appdata/Roaming/GitHub CLI'
+
+if DEFAULT_GH_CONFIG_DIR_UNIX.exists():
+    DEFAULT_GH_CONFIG_DIR = DEFAULT_GH_CONFIG_DIR_UNIX
+elif DEFAULT_GH_CONFIG_DIR_WINDOWS.exists():
+    DEFAULT_GH_CONFIG_DIR = DEFAULT_GH_CONFIG_DIR_WINDOWS
+else:
+    raise NotFoundError('No GitHub CLI config directory found')
+
+DEFAULT_GH_CONFIG_YML_PATH = DEFAULT_GH_CONFIG_DIR / 'config.yml'
+DEFAULT_GH_HOSTS_YML_PATH = DEFAULT_GH_CONFIG_DIR / 'hosts.yml'
