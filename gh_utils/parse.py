0697c25bd7a9541d10989c3e94f30d047691879b
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-06-26 22:54:18 +0800

    update parse.py

--- gh_utils/parse.py
+++ gh_utils/parse.py
@@ -7,10 +7,10 @@ def unparse_gh_remote_url(username: str, repo_name: str, use_ssh=False) -> str:
         return f"https://github.com/{username}/{repo_name}.git"
 
 
-def parse_gh_remote_url(gh_remote_url: str) -> tuple[str, str]:
+def parse_gh_remote_url(gh_remote_url: str) -> tuple[str, str, bool]:
     pattern = re.compile(r"(?:https?://github\.com/|git@github\.com:)([^/]+)/(.+)(\.git)?")
     match = pattern.match(gh_remote_url)
     if match:
-        return match.group(1), match.group(2).removesuffix('.git')
+        return match.group(1), match.group(2).removesuffix('.git'), gh_remote_url.startswith('git@')
     else:
         raise ValueError("Invalid GitHub remote URL")
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -6,7 +6,7 @@ def test_unparse_gh_remote_url():
     assert unparse_gh_remote_url("tddschn", "gh-utils", use_ssh=False) == "https://github.com/tddschn/gh-utils.git"
     
 def test_parse_gh_remote_url():
-    assert parse_gh_remote_url("git@github.com:tddschn/gh-utils.git") == ("tddschn", "gh-utils")
-    assert parse_gh_remote_url("https://github.com/tddschn/gh-utils.git") == ("tddschn", "gh-utils")
-    assert parse_gh_remote_url("git@github.com:tddschn/gh-utils") == ("tddschn", "gh-utils")
-    assert parse_gh_remote_url("https://github.com/tddschn/gh-utils") == ("tddschn", "gh-utils")
+    assert parse_gh_remote_url("git@github.com:tddschn/gh-utils.git") == ("tddschn", "gh-utils", True)
+    assert parse_gh_remote_url("https://github.com/tddschn/gh-utils.git") == ("tddschn", "gh-utils", False)
+    assert parse_gh_remote_url("git@github.com:tddschn/gh-utils") == ("tddschn", "gh-utils", True)
+    assert parse_gh_remote_url("https://github.com/tddschn/gh-utils") == ("tddschn", "gh-utils", False)
