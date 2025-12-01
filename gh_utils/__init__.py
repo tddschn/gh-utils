e4572d53b294c1197d5ee63fb9eeb1097188d85d
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2025-12-01 13:56:42 +0800

    Bump version: 0.6.9 → 1.0.0

--- gh_utils/__init__.py
+++ gh_utils/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.6.9'
+__version__ = '1.0.0'
 __app_name__ = 'gh-utils'
 
 class NotFoundError(Exception):
--- tests/test_gh_utils.py
+++ tests/test_gh_utils.py
@@ -2,4 +2,4 @@ from gh_utils import __version__
 
 
 def test_version():
-    assert __version__ == '0.6.9'
+    assert __version__ == '1.0.0'
